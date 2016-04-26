2c2b817ec4012025db9bf13446cda0cd5028fe55
Vincent Rolea <vincent.rolea@gmail.com>
2016-04-26 15:08:30 +0200

    installed simple form with bootsrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
-
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
 
   root to: 'pages#home'
-
 end
