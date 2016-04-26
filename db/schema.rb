1734014d2db8b55612680d7ddd58715c476ddbf3
Vincent Rolea <vincent.rolea@gmail.com>
2016-04-26 16:32:38 +0200

    created Gist model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160426131009) do
+ActiveRecord::Schema.define(version: 20160426143203) do
+
+  create_table "gists", force: :cascade do |t|
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.string   "description", limit: 255
+    t.string   "title",       limit: 255
+    t.string   "code",        limit: 255
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
