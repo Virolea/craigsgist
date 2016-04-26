7c22e2d41f9909de19d566041512bf69b5b80019
Vincent Rolea <vincent.rolea@gmail.com>
2016-04-26 18:57:03 +0200

    remove pundit verifications from home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+  skip_after_action :verify_authorized, unless: :devise_controller?
+  skip_after_action :verify_policy_scoped, unless: :devise_controller?
 
   def home
   end
