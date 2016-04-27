df6048db1572551941a8af6321ca1d5bea4af3e5
Vincent Rolea <vincent.rolea@gmail.com>
2016-04-27 23:59:17 +0200

    starte github upload

---
+++ app/assets/javascripts/github.js
@@ -0,0 +1,35 @@
+var title = $('.gist-title');
+var description = $('.gist-description');
+var content = editor.getSession().getValue();
+
+function getGistDataAsJson() {
+  return JSON.stringify({
+    "description": description,
+    "public": true,
+    "files": {
+      title: { "content": content }
+    }
+  });
+}
+
+function createGist() {
+  $.ajax({
+    type: "POST",
+    url: "https://api.github.com/gists",
+    data: getGistDataAsJson(),
+    success: function(data) {
+      $('#result').removeClass('error').html(
+        "Successfully created gist at " + data.html_url);
+    },
+    error: function(jqXHR) {
+      $('#result').addClass('error').html(jqXHR.responseText);
+    }
+  });
+}
+
+$(document).ready(function() {
+  $("#create-gist").submit(function(e) {
+    e.preventDefault();
+    createGist();
+  });
+});
